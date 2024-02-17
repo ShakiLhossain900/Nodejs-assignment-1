a74e49418b337f660cb33e40e53b0aac0a1f3004
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-17 14:44:37 +0600

    the basic work completed with pub file

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const express = requre("express");
+const express = require("express");
 
 const app = express();
+const bodyParser = require("body-parser");
+
+const users = [];
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({extended: false})); //it will help income data for us
 
 app.get("/", (req, res, next) => {
-  res.render();
+  res.render("index", { pageTitle: "Add User" }); //users:users array er output
 });
 
 app.get("/users", (req, res, next) => {
-  res.render();
+  res.render("users", { pageTitle: "User" , users: users}); //here will be users.pug file and for this file i also send the pageTitle aslo
 });
 
 app.post("/add-user", (req, res, next) => {
+  users.push({name: req.body.username});
   res.redirect("/users");
 });
 
