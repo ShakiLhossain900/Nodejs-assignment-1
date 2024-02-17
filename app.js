c29c3e5e4ca9e8f391d242235a151857591504f6
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-17 16:07:26 +0600

    finally solved the error of the assignment

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const expressHbs = require("express-handlebars");
+
+// const app = express();
+
+// const users = [];
+
+// app.engine("hbs", expressHbs({ defaultLayout: "main-layout", extname: "hbs" }));
+// app.set("view engine", "hbs");
+// app.set("views", "views");
+
+// app.use(bodyParser.urlencoded({ extended: false })); //it will help income data for us
+
+// app.get("/", (req, res, next) => {
+//   res.render("index", { pageTitle: "Add User" }); //users:users array er output
+// });
+
+// app.get("/users", (req, res, next) => {
+//   res.render("users", {
+//     pageTitle: "User",
+//     users: users,
+//     hasUsers: users.length > 0,
+//   }); //here will be users.pug file and for this file i also send the pageTitle aslo
+// });
+
+// app.post("/add-user", (req, res, next) => {
+//   users.push({ name: req.body.username });
+//   res.redirect("/users");
+// });
+
+// app.listen(3000);
+
+// work with ejs
 const express = require("express");
 const bodyParser = require("body-parser");
 const expressHbs = require("express-handlebars");
@@ -5,15 +39,21 @@ const expressHbs = require("express-handlebars");
 const app = express();
 
 const users = [];
+app.engine(
+  "handlebars",
+  expressHbs.engine({
+    layoutsDir: "views/", // directory to handlebars files
+    defaultLayout: null,
+    extname: "handlebars",
+  })
+);
+app.set("view engine", "ejs");
+app.set("views", "views"); // optional
 
-app.engine("hbs", expressHbs({ defaultLayout: "main-layout", extname: "hbs" }));
-app.set("view engine", "hbs");
-app.set("views", "views");
-
-app.use(bodyParser.urlencoded({ extended: false })); //it will help income data for us
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res, next) => {
-  res.render("index", { pageTitle: "Add User" }); //users:users array er output
+  res.render("index", { pageTitle: "Add User" });
 });
 
 app.get("/users", (req, res, next) => {
@@ -21,7 +61,7 @@ app.get("/users", (req, res, next) => {
     pageTitle: "User",
     users: users,
     hasUsers: users.length > 0,
-  }); //here will be users.pug file and for this file i also send the pageTitle aslo
+  });
 });
 
 app.post("/add-user", (req, res, next) => {
