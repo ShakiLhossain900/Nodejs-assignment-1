3d85a719d4c36673c5ddde8559a04d81f0094d93
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-17 15:53:24 +0600

    getting the error of hbs

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
-const bodyParser = require("body-parser");
 
 const users = [];
 
-app.set("view engine", "pug");
+app.engine("hbs", expressHbs({ defaultLayout: "main-layout", extname: "hbs" }));
+app.set("view engine", "hbs");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false})); //it will help income data for us
+app.use(bodyParser.urlencoded({ extended: false })); //it will help income data for us
 
 app.get("/", (req, res, next) => {
   res.render("index", { pageTitle: "Add User" }); //users:users array er output
 });
 
 app.get("/users", (req, res, next) => {
-  res.render("users", { pageTitle: "User" , users: users}); //here will be users.pug file and for this file i also send the pageTitle aslo
+  res.render("users", {
+    pageTitle: "User",
+    users: users,
+    hasUsers: users.length > 0,
+  }); //here will be users.pug file and for this file i also send the pageTitle aslo
 });
 
 app.post("/add-user", (req, res, next) => {
-  users.push({name: req.body.username});
+  users.push({ name: req.body.username });
   res.redirect("/users");
 });
 
